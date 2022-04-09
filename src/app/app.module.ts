9ed41aab2099cfa679683cc05c23a5dce00c8fb2
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-04-09 12:46:19 +0300

    grnerating quote and details components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
