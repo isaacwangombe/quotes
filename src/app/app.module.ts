be7dc49f847d6ca1aff09c8d039ab1472ab6fa41
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-04-09 14:07:06 +0300

    Generating new directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
